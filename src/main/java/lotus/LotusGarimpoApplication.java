f26e8511629e29f8789788ff1ba0fc8f961e25c1
Rafael Durval <rafdurval1@gmail.com>
2026-02-21 18:42:52 -0300

    Contrução do front

---
+++ src/main/java/lotus/LotusGarimpoApplication.java
@@ -0,0 +1,13 @@
+package lotus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotusGarimpoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LotusGarimpoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/lotus/controllers/ProdutoController.java
@@ -0,0 +1,14 @@
+package lotus.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProdutoController {
+
+    @GetMapping("/produtos")
+    public String produto() {
+        return "produtos"; 
+       
+    }
+}
