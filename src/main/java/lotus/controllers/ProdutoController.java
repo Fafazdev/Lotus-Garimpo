2c8211c99c027285b641c23381dde9897350b263
MatheusIFSP <eudolly19@gmail.com>
2026-02-27 17:10:25 -0300

    Ao adicionar produto, aparece na página produtos

--- src/main/java/lotus/controllers/ProdutoController.java
+++ src/main/java/lotus/controllers/ProdutoController.java
@@ -1,14 +1,23 @@
 package lotus.controllers;
 
+import lotus.model.Produto;
+import lotus.repositories.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
 
 @Controller
 public class ProdutoController {
 
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
     @GetMapping("/produtos")
-    public String produto() {
+    public String produto(Model model) {
+        List<Produto> produtos = produtoRepository.findAll();
+        model.addAttribute("produtos", produtos);
         return "produtos"; 
-       
     }
 }
